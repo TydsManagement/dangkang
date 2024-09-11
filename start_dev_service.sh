e73ac7e8f8ba3c42185c3a30362afb7ae374cf96
十方空烬 <xe333@sina.com>
2024-09-11 14:20:28 +0800

    修正了激活Conda环境的命令
    使用`source`命令替换了之前的Conda激活方式，以确保正确地激活了`ragflow`Conda环境。此更改对于在系统中正确设置环境变量和依赖项至关重要，从而可以顺利运行`ragflow-start.sh`脚本。

--- start_dev_service.sh
+++ start_dev_service.sh
@@ -20,6 +20,12 @@ WorkingDirectory=$INSTALL_DIR
 ExecStart=/bin/bash -c "source /home/hnty/anaconda3/bin/activate ragflow && /bin/bash $INSTALL_DIR/ragflow-start.sh"
 Restart=always
 Environment="WS=2" "API_PORT=8000" "API_HOST=0.0.0.0" "API_TIMEOUT=30" "API_MAX_CONN=100" "API_MAX_RETRY=3" "API_RETRY_INTERVAL=10" "API_RETRY_TIMEOUT=5" "API_RETRY_MAX_CONN=100" "API_RETRY_MAX_RETRY=3" "API_RETRY_RETRY_INTERVAL=10" "API_RETRY_RETRY_TIMEOUT=5"
+Environment="STACK_VERSION=8.11.3" "ES_PORT=1200" "ELASTIC_PASSWORD=infini_rag_flow"
+Environment="KIBANA_PORT=6601" "KIBANA_USER=rag_flow" "KIBANA_PASSWORD=infini_rag_flow"
+Environment="MEM_LIMIT=8073741824" "MYSQL_PASSWORD=infini_rag_flow" "MYSQL_PORT=5455"
+Environment="MINIO_CONSOLE_PORT=9001" "MINIO_PORT=9000" "MINIO_USER=rag_flow" "MINIO_PASSWORD=infini_rag_flow"
+Environment="REDIS_PORT=6379" "REDIS_PASSWORD=infini_rag_flow" "SVR_HTTP_PORT=9527"
+Environment="RAGFLOW_VERSION=dev" "TIMEZONE='Asia/Shanghai'"
 
 [Install]
 WantedBy=multi-user.target
